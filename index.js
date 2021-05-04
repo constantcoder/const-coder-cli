c5f1e9399127297acabd82a0fd8a1440bca7f860
constantcoder <garrett@constantcoder.io>
2021-05-04 14:05:50 -0500

    📦 NEW: First version

---
+++ index.js
@@ -0,0 +1,10 @@
+#!/usr/bin/env node
+
+console.log(`
+Style Spin
+
+A command line interface to help you spin up your new project with the framework, library and styling you want.
+
+Made by @CoderConstant
+
+`);
